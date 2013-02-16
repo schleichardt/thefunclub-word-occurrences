6b3b846344c291783267b370c5cf2533a60ee4db
Michael Schleichardt <michael@schleichardt.net>
2013-02-16 22:05:01 +0100

    refactoring

--- src/main/scala/WordOccurrencesMain.scala
+++ src/main/scala/WordOccurrencesMain.scala
@@ -6,11 +6,13 @@ object WordOccurrencesMain extends App {
 
 //nice to know: after dropWhile/takeWhile the original iterator should not be used,else: undefined behaviour
 class WordIterator(chars: Iterator[Char]) extends Iterator[String] {
-  private var internalIterator = chars.map(_.toLower).map(c => if (c >= 'a' && c <= 'z') c else ' ').dropWhile(_ == ' ')
+  private def isWordCharacter(c: Char) = c >= 'a' && c <= 'z'
+  private def isNotWordCharacter(c: Char) = !isWordCharacter(c)
+  private var internalIterator = chars.map(_.toLower).dropWhile(isNotWordCharacter)
   def hasNext = internalIterator.hasNext
   def next = {
-    val (iteratorWithWord, iteratorWithLeftOver) = internalIterator.span(_ != ' ')
-    internalIterator = iteratorWithLeftOver.dropWhile(_ == ' ')
+    val (iteratorWithWord, iteratorWithLeftOver) = internalIterator.span(isWordCharacter)
+    internalIterator = iteratorWithLeftOver.dropWhile(isNotWordCharacter)
     iteratorWithWord.mkString
   }
 }
